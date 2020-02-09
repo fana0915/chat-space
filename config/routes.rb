9fedd16dd45a08a4fd8778f69d47659af88d1d9f
YourName <you@example.com>
2020-02-09 15:36:46 +0900

    create-massage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "messages#index"
-  resources :users, only: [:edit, :update, :destroy]
+  root "groups#index"
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
